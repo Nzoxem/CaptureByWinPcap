e352b542725b7a954c941f45ecc3c7c2c46e6dfc
eris <zxeris@foxmail.com>
2018-03-29 15:41:08 +0800

    完成各种协议格式的封装

--- WinpcapTs/capture.cpp
+++ WinpcapTs/capture.cpp
@@ -9,6 +9,81 @@
 #define MAX_ADDR_LEN 16
 FILE *file = 0;//用来保存抓包结果
 
+//对各个协议的数据结构进行封装
+
+//以太网协议格式
+typedef struct ether_header{
+	u_char ether_dhost[6];	//目的mac地址
+	u_char ether_shost[6];	//源mac地址
+	u_short ether_type;		//以太网类型
+}ether_header;
+//IP地址
+typedef struct ip_address{
+	u_char byte1;
+	u_char byte2;
+	u_char byte3;
+	u_char byte4;
+}ip_address;
+
+//IPV4数据报头部
+typedef struct ip_header{
+	u_char version_hlen;	//首部长度、版本
+	u_char tos;				//服务质量
+	u_short tlen;			//总长度
+	u_short identification;	//身份识别
+	u_short flags_offset;	//标识 分钟偏移
+	u_char ttl;				//生存时间
+	u_char proto;			//协议类型
+	u_short checksum;		//头部校验和
+	u_int saddr;			//源IP地址
+	u_int daddr;			//目的IP地址
+}ip_header;
+
+//TCP头部
+typedef struct tcp_header{
+	u_short sport;			//源端口号
+	u_short dport;			//目的端口号
+	u_int sequence;			//序列码
+	u_int ack;				//回复码
+	u_char hdrLen;			//首部长度 保留字
+	u_char flags;			//标志
+	u_short windows;		//窗口大小
+	u_short checksum;		//校验和
+	u_short urgent_p;		//紧急指针
+}tcp_header;
+
+//UDP头部
+typedef struct udp_header{
+	u_short sport;			//源端口号
+	u_short dport;			//目的端口号
+	u_short datalen;		//数据长度
+	u_short checksum;		//校验和
+}udp_header;
+
+//ICMP头部
+typedef struct icmp_header{
+	u_char type;			//ICMP类型
+	u_char code;			//代码
+	u_short checksum;		//校验和
+	u_short identification;	//标识
+	u_short sequence;		//序列号
+	u_long timestamp;		//时间戳
+}icmp_header;
+
+//ARP头部
+typedef struct arp_header{
+	u_short hardware_type;	//格式化的硬件地址
+	u_short protocol_type;	//地址协议格式
+	u_char hardware_length;	//硬件地址长度
+	u_char protocol_length;	//地址协议长度
+	u_short operation_code;	//操作码
+	u_char source_ethernet_address[6];//发送者硬件地址
+	u_char source_ip_address[4];//发送者协议地址
+	u_char destination_ethernet_address[6];//目的方硬件地址
+	u_char destination_ip_address[4];//目的方协议地址
+}arp_header;
+
+
 int main(){
 	
 	pcap_if_t *alldevs;//pcap_if_t是一个链表的数据结构，表明网络接口的信息，
